e204eb03e751ef1977efaec499644ccf9f2e0070
Łukasz Ostrowski <lukasz.ostrowski@balluff.pl>
2018-09-26 09:56:22 +0200

    Add project files.

---
+++ Zadanie_Punkt/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_Punkt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 50;
+            Console.WindowWidth = 84;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Title = "BALLUFF - KURS C#";
+            string kursor = "BALLUFF";
+
+            Punkt punkt = new Punkt();
+
+            
+            
+            // punkt.Gora(1);
+            // punkt.Lewo(2);
+
+
+            while (true)
+            {
+                ConsoleKeyInfo klawisz = Console.ReadKey();
+                switch (klawisz.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        Console.Clear(); punkt.Gora(1); punkt.Wspolrzedne();
+                        Console.SetCursorPosition(punkt.PozX, punkt.PozY);
+                        Console.WriteLine(kursor);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.Clear(); punkt.Dol(1); punkt.Wspolrzedne();
+                        Console.SetCursorPosition(punkt.PozX, punkt.PozY);
+                        Console.WriteLine(kursor);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.Clear(); punkt.Lewo(1)  ; punkt.Wspolrzedne();
+                        Console.SetCursorPosition(punkt.PozX, punkt.PozY);
+                        Console.WriteLine(kursor);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.Clear(); punkt.Prawo(1); punkt.Wspolrzedne();
+                        Console.SetCursorPosition(punkt.PozX, punkt.PozY);
+                        Console.WriteLine(kursor);
+                        break;
+
+                }
+            }
+            
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zadanie_Punkt/Punkt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_Punkt
+{
+    class Punkt
+    {
+        public Punkt()
+        {
+            PozX = 20;
+            PozY = 20;
+            Console.WriteLine("Utworzono punkt");
+            Wspolrzedne();
+        }
+
+        public int PozX { get; private set; }
+        public int PozY { get; private set; }
+
+        public int Gora(int G)
+        {
+            return PozY = PozY-G;
+        }
+
+        public int Dol(int D)
+        {
+            return PozY = PozY + D;
+        }
+
+        public int Lewo(int L)
+        {
+            return PozX = PozX -L;
+        }
+
+        public int Prawo(int P)
+        {
+            return PozX = PozX + P;
+        }
+
+
+        public void Wspolrzedne()
+        {
+            Console.WriteLine("Wspolrzedna x = {0} Wspolrzedna y = {1}",PozX , PozY);
+
+        }
+    }
+}
